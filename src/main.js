52f67b034bd12aa9d4da35feece9c6b4a7d4914c
sheldon <sotyoyotao@gmail.com>
2020-07-25 07:30:04 +0800

    finish it

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 function handleInputKey(e) {
-  // console.log(`- handleInputKey `, e);
   if (e.keyCode === 13) {
     handleAddTodoEvent(e);
   }
@@ -7,26 +6,74 @@ function handleInputKey(e) {
 
 function getInputValue() {
   let oInput = document.querySelector(".input-trigger");
-  return oInput.target.value;
+  return oInput.value;
+}
+
+function clearInput() {
+  let oInput = document.querySelector(".input-trigger");
+  oInput.value = "";
+}
+
+function createCheckbox() {
+  let oCheckbox = document.createElement("input");
+  oCheckbox.setAttribute("type", "checkbox");
+  oCheckbox.className = "check-trigger";
+  oCheckbox.onclick = handleCheckTodoEvent;
+  return oCheckbox;
+}
+
+function createEventContent(text) {
+  let oP = document.createElement("p");
+  oP.innerText = text;
+  oP.className = "event-content";
+  return oP;
+}
+
+function createDeleteButton() {
+  let oDelete = document.createElement("input");
+  oDelete.setAttribute("type", "button");
+  oDelete.className = "delete-trigger";
+  oDelete.value = "x";
+  oDelete.onclick = handleRemoveTodoEvent;
+  return oDelete;
+}
+
+function createOneTodoEvent() {
+  let id = new Date().getTime();
+  let oLi = document.createElement("li");
+  oLi.setAttribute("data-id", id);
+  let oCheckbox = createCheckbox();
+  let oContent = createEventContent(getInputValue());
+  let oDelete = createDeleteButton();
+  oLi.appendChild(oCheckbox);
+  oLi.appendChild(oContent);
+  oLi.appendChild(oDelete);
+  return oLi;
 }
 
 // ## 行为
 // 1. add todo
 function handleAddTodoEvent(e) {
-  console.log(`- handleAddTodoEvent `, e);
+  let oLi = createOneTodoEvent();
+  let oList = document.querySelector("ul");
+  oList.appendChild(oLi);
+  clearInput();
 }
 
 // 2. check todo
 function handleCheckTodoEvent(e) {
-  console.log(`- handleCheckTodoEvent `, e);
+  let checked = e.target.checked;
+  let parentNode = e.target.parentNode;
+  if (checked) {
+    parentNode.classList.add("completed");
+  } else {
+    parentNode.classList.remove("completed");
+  }
 }
 
-// 3. uncheck todo
-function handleUncheckTodoEvent(e) {
-  console.log(`- handleUncheckTodoEvent `, e);
-}
-
-// 4. remove todo
+// 3. remove todo
 function handleRemoveTodoEvent(e) {
-  console.log(`- handleRemoveTodoEvent `, e);
+  let parentNode = e.target.parentNode;
+  let grandParentNode = parentNode.parentNode;
+  grandParentNode.removeChild(parentNode);
 }
